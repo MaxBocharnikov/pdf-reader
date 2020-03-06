907110e619719ad9ae8db4feeb2d086be7f03c80
MaxBocharnikov <mbocharnikov@themsteam.com>
2020-03-06 09:05:52 +0700

    add base_url constant

---
+++ src/constants/http.client.js
@@ -0,0 +1,1 @@
+export const _BASE_URL = process.env.REACT_APP_SERVER_URL;
--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,11 +1,12 @@
 import HttpClient from './http.client';
+import {_BASE_URL} from '../../constants/http.client';
 
 export default class Api {
 
     static getFileData = async (file) => {
         const formData = new FormData();
         formData.append('file', file);
-        const response = await HttpClient.call('post',`http://kolbasa/wapi/documents`, formData);
+        const response = await HttpClient.call('post', `${_BASE_URL}/documents`, formData);
         return response.data;
     }
 }
